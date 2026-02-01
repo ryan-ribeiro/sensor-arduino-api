ce6bc7f99365f1e9e577cdfa1c954ca17179630d
ryan-ribeiro <ryanrodrigues0071234@gmail.com>
2026-02-01 18:15:14 -0300

    fix: update Dockerfile to conditionally run tests based on TESTCONTAINERS_DISABLED argument

--- sensor-api/src/test/java/com/github/ryanribeiro/sensor/SensorApiApplicationTests.java
+++ sensor-api/src/test/java/com/github/ryanribeiro/sensor/SensorApiApplicationTests.java
@@ -3,10 +3,13 @@ package com.github.ryanribeiro.sensor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.containers.PostgreSQLContainer;
+
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.AfterAll;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
+@Tag("integration")
 @SpringBootTest
 class SensorApiApplicationTests {
 
